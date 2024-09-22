10954f8e1d2500e7212dc3770e25ad803f09a235
shinabzo <shinabzo@student.21-school.ru>
2024-09-22 15:40:53 +0000

    123

--- src/ssh.sh
+++ src/ssh.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-echo "Current directory: $(pwd)"
-echo "Contents of the directory:"
-ls -la part2/cat/
-ls -la part2/grep/
-
-scp part2/cat/s21_cat server@10.10.0.2:~/
-scp part2/grep/s21_grep server@10.10.0.2:~/
+scp /home/gitlab-runner/builds/jb2D3yF-/0/students/DO6_CICD.ID_356283/shinabzo_student.21_school.ru/DO6_CICD-1/src/part2/cat/s21_cat server@10.10.0.2:~/
+scp /home/gitlab-runner/builds/jb2D3yF-/0/students/DO6_CICD.ID_356283/shinabzo_student.21_school.ru/DO6_CICD-1/src/part2/grep/s21_grep server@10.10.0.2:~/
 ssh server@10.10.0.2 "echo 123 | sudo -S mv s21_cat /usr/local/bin"
 ssh server@10.10.0.2 "echo 123 | sudo -S mv s21_grep /usr/local/bin"
